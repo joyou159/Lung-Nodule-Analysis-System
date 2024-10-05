d9124dc0eb24214f20719aa0c68f181566d7901d
Youssef Shawki <ya0721435@gmail.com>
2024-10-06 00:12:37 +0300

    GPU augmentation functionality

--- segment_dset_utils.py
+++ segment_dset_utils.py
@@ -2,11 +2,14 @@ import functools
 import glob
 import os
 import csv
+import torch.nn as nn
+import torch 
 from collections import namedtuple
+import random
+from math import sin, cos, pi
 
 
 CandidateSegInfo = namedtuple('CandidateSegInfo', 'is_nodule, hasAnnotation_bool, isMal_bool, diameter_mm, series_uid, center_xyz')
-
 
 @functools.lru_cache(maxsize=1)  # caches the results of function call, if it have been called with the same argument. 
 def get_candidate_info_list(dataset_dir_path, required_on_desk=True, subsets_included = (0,1,2,3,4)):
@@ -101,3 +104,81 @@ def get_candidate_info_dict(dataset_dir_path, required_on_desk=True, subsets_inc
     
     return candidate_dict
 
+
+"""
+The main purpose of this class is to make the preprocess step of data augmentation to be done on the GPUs.
+"""
+class SegmentationAugmentation(nn.Module):
+    def __init__(self, flip=None, offset=None, scale=None, rotate=None, noise=None):
+        super().__init__()
+
+        self.flip = flip 
+        self.offset = offset 
+        self.scale = scale 
+        self.rotate = rotate
+        self.noise = noise 
+
+
+    def forward(self, input_batch, mask_batch):
+        """
+        input_batch: batch of 2D images of spatial shape (height, width)
+        mask_batch: batch of 2D images of spatial shape (height, width)
+
+        """
+        transform_mat = self.build_2d_transformation_matrix()
+        transform_mat = transform_mat.expand(input_batch.shape[0], -1, -1) # account for batch dimension 
+        transform_mat = transform_mat.to(input_batch.device, torch.float32) # loading the matrix to GPU
+        
+        transform_grid  = nn.functional.affine_grid(transform_mat[:,:2], # when defining the grid, we just take the first two rows
+                                                      input_batch.size(), align_corners=False) 
+
+        augmented_input = nn.functional.grid_sample(input_batch, 
+                                                    transform_grid, 
+                                                    padding_mode="border", 
+                                                    align_corners=False)
+        
+        augmented_masks = nn.functional.grid_sample(mask_batch.to(torch.float32),  # because by default it's (torch.long), inconvenient    
+                                                    transform_grid, 
+                                                    padding_mode="border", 
+                                                    align_corners=False)
+
+        if self.noise:
+            noise_added = torch.rand_like(augmented_input) * self.noise
+            augmented_input += noise_added
+
+        return augmented_input, augmented_masks > 0.5 # convert back to boolean. 
+
+    def build_2d_transformation_matrix(self):
+        transform_mat = torch.eye(3) 
+
+        for i in range(2):
+            if self.flip:
+                if random.random() > 0.5: # flipping is a bit random 
+                    transform_mat[i:i] *= -1
+
+            if self.offset:
+                offset_value = self.offset # must be limited [-1,1]
+                random_factor = (random.random() * 2 - 1)  # (std -> 2) and (mean -> -1)
+                transform_mat[i,2] = offset_value * random_factor # the grid_sample will interpolate since the transition won't be in voxel steps 
+
+            if self.scale:
+                scaling_value = self.scale
+                random_factor = (random.random() * 2 - 1)
+                transform_mat[i:i] *= 1 + scaling_value * random_factor        
+
+        if self.rotate:
+            rotation_angle = random.random() * pi * 2
+            s = sin(rotation_angle)
+            c = cos(rotation_angle)
+
+            rotation_matrix = torch.tensor([
+                [c, -s, 0],
+                [s, c, 0],
+                [0, 0, 1]
+            ])
+
+            transform_mat @= rotation_matrix
+    
+        return transform_mat
+
+    
