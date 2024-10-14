7b8f69304b23d2075d59e5876b836b2ca56928b9
Youssef Shawki <ya0721435@gmail.com>
2024-10-14 12:21:44 +0300

    3D augmentation on GPU functionality

--- classifier_dset_utils.py
+++ classifier_dset_utils.py
@@ -1,15 +1,137 @@
 from collections import namedtuple
 import torch 
 import random
-import functools
 from common_utils.util import *
 from common_utils.disk_caching import * 
-import os
-import csv
 import numpy as np 
-import glob
 import torch.nn as nn 
 from CT import get_ct, get_ct_raw_candidates
+
+
+class AugmentationCandidate(nn.Module):
+    def __init__(self, augmentation_dict):
+        super().__init__()
+
+        self.flip = augmentation_dict.flip 
+        self.offset = augmentation_dict.offset 
+        self.scale = augmentation_dict.scale 
+        self.rotate = augmentation_dict.rotate
+        self.noise = augmentation_dict.noise 
+
+
+    def forward(self, series_uid, center_xyz, width_irc, use_cache = True):
+
+        if use_cache:
+            ct_chunks, center_irc = get_ct_raw_candidates(series_uid, center_xyz, width_irc) 
+        else:
+            ct = get_ct(series_uid)
+            ct_chunks, center_irc = ct.get_raw_candidate_nodule(center_xyz, width_irc)
+
+        ct_tensor = torch.tensor(ct_chunks).unsqueeze(0).unsqueeze(0).to("cuda",torch.float32) # the (batch_size, channel, depth, heigth, width)  the expected shape of pytorch 
+        
+        transform_mat = self.build_3d_transformation_matrix()
+        transform_mat = transform_mat.to(ct_tensor.device, torch.float32) # loading the matrix to GPU
+        
+        affine_transform = nn.functional.affine_grid(transform_mat[:3].unsqueeze(0), # transformation_mat[:3] coordinates only (not density value) 
+                                                 ct_tensor.size(), align_corners=False)
+    
+            
+        augmented_chunk = nn.functional.grid_sample(ct_tensor, affine_transform, padding_mode="border", align_corners=False)
+
+        if self.noise:
+            noise_added = torch.rand_like(augmented_chunk) * self.noise
+            augmented_chunk += noise_added
+
+        return augmented_chunk[0], center_irc
+
+    def build_3d_transformation_matrix(self):
+        transformation_mat = torch.eye(4) # start off (just identity)
+
+        for i in range(3): # per axis 
+            if self.flip:
+                if random.random() > 0.3: # flipping is a bit random 
+                    transformation_mat[i,i] *= -1
+
+            if self.offset:
+                offset_value = self.offset # must be limited [-1,1]
+                random_factor = (random.random() * 2 - 1)  # (std -> 2) and (mean -> -1)
+                transformation_mat[i,3] = offset_value * random_factor # the grid_sample will interpolate since the transition won't be in voxel steps 
+
+            if self.scale:
+                scaling_value = self.scale
+                random_factor = (random.random() * 2 - 1)
+                transformation_mat[i,i] *= 1 + scaling_value * random_factor
+    
+        # rotation around z-axis (because the scale of this axis is completely different from those of x & y)
+        if self.rotate:
+            angle_in_rad = random.random() * np.pi * 2
+            s = np.sin(angle_in_rad)
+            c = np.cos(angle_in_rad)
+
+            rotation_mat = torch.tensor(
+                [
+                    [c, -s, 0, 0],
+                    [s, c, 0, 0],
+                    [0, 0, 1, 0],
+                    [0, 0, 0, 1]
+                ]
+            ) 
+
+            transformation_mat @= rotation_mat.to(torch.float32) # accumulate the transformation matrices
+
+    
+        return transformation_mat
+
+def augmentation_3D(input_batch, augmentation_dict):
+    
+    transform_t = torch.eye(4, dtype=torch.float32)
+    for i in range(3):
+        if augmentation_dict["flip"]: 
+            if random.random() > 0.5:
+                transform_t[i,i] *= -1
+                
+        if augmentation_dict["offset"]: 
+            offset_float = augmentation_dict["offset"]
+            random_float = (random.random() * 2 - 1)
+            transform_t[3,i] = offset_float * random_float
+            
+        if augmentation_dict["scale"]:
+            scaling_value = augmentation_dict["scale"]
+            random_factor = (random.random() * 2 - 1)
+            transform_t[i,i] *= 1 + scaling_value * random_factor
+            
+    if augmentation_dict["rotate"]:
+        angle_rad = random.random() * np.pi * 2
+        s = np.sin(angle_rad)
+        c = np.cos(angle_rad)
+
+        rotation_t = torch.tensor([
+            [c, -s, 0, 0],
+            [s, c, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1],
+        ], dtype=torch.float32)
+
+        transform_t @= rotation_t
+        
+    affine_t = torch.nn.functional.affine_grid(
+            transform_t[:3].unsqueeze(0).expand(input_batch.size(0), -1, -1).to(input_batch.device),
+            input_batch.shape,
+            align_corners=False,
+        )
+
+    augmented_chunk = torch.nn.functional.grid_sample(
+            input_batch,
+            affine_t,
+            padding_mode='border',
+            align_corners=False,
+        )
+    if augmentation_dict["noise"]:
+        noise_t = torch.randn_like(augmented_chunk)
+        noise_t *= augmentation_dict['noise']
+        augmented_chunk += noise_t
+        
+    return augmented_chunk
 
 
 
@@ -29,14 +151,12 @@ def get_unified_candidate_info_list(dataset_dir_path, required_on_desk=True, sub
     """
     
     mhd_list = glob.glob("/kaggle/input/luna16/subset*/subset*/*.mhd") # extract all 
-    if required_on_desk:
-        filtered_mhd_list = [mhd for mhd in mhd_list if any(f"subset{id}" in mhd for id in subsets_included)]
-        uids_present_on_disk = {os.path.split(p)[-1][:-4] for p in filtered_mhd_list} # the unique series_uids for further filtration
-        mhd_list = uids_present_on_disk
-
+    filtered_mhd_list = [mhd for mhd in mhd_list if any(f"subset{id}" in mhd for id in subsets_included)]
+    uids_present_on_disk = {os.path.split(p)[-1][:-4] for p in filtered_mhd_list} # the unique series_uids for further filtration
+    
     annotation_info = dict()
     
-    with open(os.path.join(dataset_dir_path, "annotations.csv"), "r") as f:
+    with open(os.path.join(dataset_dir_path, "annotations.csv")) as f:
         for row in list(csv.reader(f))[1:]: # neglect the first indexing column 
             series_uid = row[0]
             nodule_center = np.array([float(x) for x in row[1:4]]) # x, y, z 
@@ -49,7 +169,7 @@ def get_unified_candidate_info_list(dataset_dir_path, required_on_desk=True, sub
         for row in list(csv.reader(f))[1:]:
             series_uid = row[0]
             
-            if series_uid not in mhd_list:
+            if series_uid not in uids_present_on_disk and required_on_desk: 
                 continue # meaning that subject doesn't exist on the desk 
                 
             nodule_flag = bool(int(row[4])) # is it an actual nodule?
@@ -66,7 +186,7 @@ def get_unified_candidate_info_list(dataset_dir_path, required_on_desk=True, sub
                     cand_nodule_diameter = nodule_diameter 
                     break # meaning i reached to the annotation corresponding to what i need, so get out. 
             candidate_list.append(
-                CandidateInfoTuple(nodule_flag,
+                Candidate_Info(nodule_flag,
                                    cand_nodule_diameter,
                                    series_uid,
                                    cand_nodule_center))
@@ -75,63 +195,3 @@ def get_unified_candidate_info_list(dataset_dir_path, required_on_desk=True, sub
     # so overall, the returned list has the candidates that are nodules indeed ordered descendingly by their sizes  
     # after that come the non-nodule candidates
     return candidate_list
-
-
-
-@functools.lru_cache(maxsize=1)  
-def get_ct_augmented_candidates(augmentation_dict, series_uid, center_xyz, width_irc, use_cache = True):
-    if use_cache:
-        ct_chunks, center_irc = get_ct_raw_candidates(series_uid, center_xyz, width_irc, usage ="classifier") 
-    else:
-        ct = get_ct(series_uid, usage ="classifier")
-        ct_chunks, center_irc = ct.get_raw_candidate_nodule(center_xyz, width_irc)
-
-    ct_tensor = torch.tensor(ct_chunks).unsqueeze(0).unsqueeze(0).to(torch.float32) # the (batch_size, channel, depth, heigth, width)  the expected shape of pytorch 
-
-    transformation_mat = torch.eye(4) # start off (just identity)
-
-    for i in range(3): # per axis 
-        if augmentation_dict['flip']:
-            if random.random() > 0.5: # flipping is a bit random 
-                transformation_mat[i:i] *= -1
-
-        if augmentation_dict['offset']:
-            offset_value = augmentation_dict['offset'] # must be limited [-1,1]
-            random_factor = random.random() * 2 - 1  # (std -> 2) and (mean -> -1)
-            transformation_mat[i:4] = offset_value * random_factor # the grid_sample will interpolate since the transition won't be in voxel steps 
-
-        if augmentation_dict['scale']:
-            scaling_value = augmentation_dict["scale"]
-            random_factor = (random.random() * 2 - 1)
-            transformation_mat[i:i] *= 1 + scaling_value * random_factor
-    
-    # rotation around z-axis (because the scale of this axis is completely different from those of x & y)
-    if augmentation_dict['rotate']:
-        angle_in_rad = random.random() * np.pi * 2
-        s = np.sin(angle_in_rad)
-        c = np.cos(angle_in_rad)
-
-        rotation_mat = torch.tensor(
-            [
-                [c, -s, 0, 0],
-                [s, c, 0, 0],
-                [0, 0, 1, 0],
-                [0, 0, 0, 1]
-            ]
-        ) 
-                             
-        transformation_mat @= rotation_mat # accumulate the transformation matrices
-
-    affine_transform = nn.functional.affine_grid(transformation_mat[:3].unsqueeze(0).to(torch.float32), # transformation_mat[:3] coordinates only (not density value) 
-                                                 ct_tensor.size, align_corners=False) 
-    
-    augmented_chunk = nn.functional.grid_sample(ct_tensor, affine_transform, padding_mode="border", align_corners=False).to('cpu') 
-
-    
-    if augmentation_dict['noise' ]:
-        noise_t = torch.randn_like(augmented_chunk)
-        noise_t *= augmentation_dict['noise']
-        augmented_chunk += noise_t
-
-    
-    return augmented_chunk[0], center_irc # (discard the batch dim, just up to the channel dim)
